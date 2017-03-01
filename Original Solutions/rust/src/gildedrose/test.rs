86fdee49ab62639a3d6fadc3c2a484b5adfa524c
Adam <adam@hodgen.uk>
2017-03-01 19:09:57 +0000

    fix test, and lint error

--- Original Solutions/rust/src/gildedrose/test.rs
+++ Original Solutions/rust/src/gildedrose/test.rs
@@ -1,10 +1,10 @@
 use super::{Item, GildedRose};
 
 #[test]
-pub fn foo() {
+pub fn test_item_name_doesnt_change() {
     let items = vec![Item::new(String::from("foo"), 0, 0)];
     let mut rose = GildedRose::new(items);
     rose.update_quality();
 
-    assert_eq!("fixme", rose.items[0].name);
+    assert_eq!("foo", rose.items[0].name);
 }
