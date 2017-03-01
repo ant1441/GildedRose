f76bdf27dc60b4353264473bdea05c1948496f5f
Adam <adam@hodgen.uk>
2017-03-01 20:48:38 +0000

    Add type of object, and handle Conjured items specially

--- Original Solutions/rust/src/gildedrose/mod.rs
+++ Original Solutions/rust/src/gildedrose/mod.rs
@@ -7,12 +7,44 @@ pub struct Item {
     pub quality: i32,
 }
 
+enum Type {
+    Legendary,
+    Rare,
+    RareWithTime,
+    Normal,
+}
+
 impl Item {
     pub fn new(name: String, sell_in: i32, quality: i32) -> Item {
         Item {
             name: name,
             sell_in: sell_in,
             quality: quality,
+        }
+    }
+
+    fn type_of(&self) -> Type {
+        match self.name.as_ref() {
+            "Sulfuras, Hand of Ragnaros" => Type::Legendary,
+            "Backstage passes to a TAFKAL80ETC concert" => Type::RareWithTime,
+            "Aged Brie" => Type::Rare,
+            _ => Type::Normal,
+        }
+    }
+
+    fn increment_quality(&self, n: i32) -> i32 {
+        if self.name.starts_with("Conjured") {
+            self.quality + (2 * n)
+        } else {
+            self.quality + n
+        }
+    }
+
+    fn decrement_quality(&self, n: i32) -> i32 {
+        if self.name.starts_with("Conjured") {
+            self.quality - (2 * n)
+        } else {
+            self.quality - n
         }
     }
 }
@@ -28,55 +60,45 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for mut item in &mut self.items {
-            item.quality = GildedRose::handle_quality(&mut item);
-            GildedRose::decrease_sell_by(&mut item);
+            item.quality = GildedRose::handle_quality(&item);
+            item.sell_in = GildedRose::decrease_sell_by(&item);
         }
     }
 
     fn handle_quality(item: &Item) -> i32 {
-        let mut qual = item.quality;
-        match (item.name.as_ref(), qual) {
-            ("Sulfuras, Hand of Ragnaros", n) => n,
+        match (item.type_of(), item.quality) {
+            (Type::Legendary, n) => n,
             (_, n) if n >= 50 => n,
-            ("Backstage passes to a TAFKAL80ETC concert", _) => {
-                if item.sell_in <= 0 {
-                    return 0;
+            (Type::RareWithTime, _) => {
+                match item.sell_in {
+                    n if n <= 0 => 0,
+                    1...5 => item.increment_quality(3),
+                    6...10 => item.increment_quality(2),
+                    _ => item.increment_quality(1),
                 }
-                if item.sell_in < 6 {
-                    return qual + 3;
-                }
-                if item.sell_in < 11 {
-                    return qual + 2;
-                }
-                return qual + 1;
             }
-            ("Aged Brie", _) => {
-                if item.sell_in <= 0 {
-                    return qual + 2;
+            (Type::Rare, _) => {
+                match item.sell_in {
+                    n if n <= 0 => item.increment_quality(2),
+                    _ => item.increment_quality(1),
                 }
-                return qual + 1;
             }
-            (_, _) => {
-                if qual <= 0 {
-                    return qual;
-                }
-                if item.name.starts_with("Conjured") {
-                    if item.sell_in <= 0 {
-                        return qual - 3;
-                    }
-                    return qual - 2;
-                }
+            (Type::Normal, n) if n <= 0 => item.quality,
+            (Type::Normal, _) => {
                 if item.sell_in <= 0 {
-                    return qual - 2;
+                    return item.decrement_quality(2);
+                } else {
+                    return item.decrement_quality(1);
                 }
-                return qual - 1;
             }
         }
     }
 
-    fn decrease_sell_by(item: &mut Item) {
-        if item.name != "Sulfuras, Hand of Ragnaros" {
-            item.sell_in -= 1;
+    fn decrease_sell_by(item: &Item) -> i32 {
+        if item.name == "Sulfuras, Hand of Ragnaros" {
+            item.sell_in
+        } else {
+            item.sell_in - 1
         }
     }
 }
