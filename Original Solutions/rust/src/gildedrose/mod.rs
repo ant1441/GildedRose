d60877995ba2896a189825d317e471160655279c
Adam <adam@hodgen.uk>
2017-03-01 20:02:59 +0000

    Handle Brie specially

--- Original Solutions/rust/src/gildedrose/mod.rs
+++ Original Solutions/rust/src/gildedrose/mod.rs
@@ -52,25 +52,24 @@ impl GildedRose {
             }
             return;
         }
-        if item.name != "Aged Brie" {
-            if item.quality > 0 {
-                item.quality = item.quality - 1;
-                if item.name.starts_with("Conjured") {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
+        if item.name == "Aged Brie" {
             if item.quality < 50 {
                 item.quality = item.quality + 1;
+            }
+            if item.sell_in <= 0 && item.quality < 50 {
+                item.quality = item.quality + 1;
+            }
+            return;
+        }
+        if item.quality > 0 {
+            item.quality = item.quality - 1;
+            if item.name.starts_with("Conjured") {
+                item.quality = item.quality - 1;
             }
         }
 
         if item.sell_in <= 0 {
-            if item.name == "Aged Brie" {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1;
-                }
-            } else if item.quality > 0 {
+            if item.quality > 0 {
                 item.quality = item.quality - 1;
             }
         }
